bd6b2480f2c6de51664c2913249781d7a602a1e2
fdoljanin <doljaninfrane@gmail.com>
2023-03-03 12:29:27 +0100

    Finished task 1 - plot function

--- Vjezbe/Vjezbe_1/hello.py
+++ Vjezbe/Vjezbe_1/hello.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
 def tryParse(parser, object):
     try:
         return (True, parser(object))
@@ -12,11 +16,37 @@ def getLineEq(p1, p2):
     a = (p2["y"] - p1["y"]) / (p2["x"]-p1["x"])
     b = p1["y"] - p1["x"]*a
 
+    return (a, b)
+
+
+def plotGraph(function=None, title=None, dots=[], decision='PDF'):
+    if function:
+        minX = min(map(lambda e: e['x'], dots))
+        maxX = max(map(lambda e: e['x'], dots))
+
+        x = np.linspace(min(-5, minX-1), max(5, maxX+1), 100)
+        y = function(x)
+        plt.plot(x, y)
+
+    for dot in dots:
+        plt.plot(dot['x'], dot['y'], 'bo')
+
+    if title:
+        plt.title(title)
+
+    if decision == 'PDF':
+        plt.savefig('plot.pdf')
+    elif decision == 'SHOW':
+        plt.show()
+    else:
+        raise Exception('Invalid show option')
+
+
+def getFunctionName(a, b):
     return f"{a}x {'+' if b>=0 else '-'} {abs(b)}"
 
 
 coords = None
-
 while True:
     userInput = input("Unesi koordinate u obliku x1,y1 x2,y2")
     isSuccess, coords = tryParse(lambda i: lmap(lambda c: dict(
@@ -25,4 +55,6 @@ while True:
         break
     print("Unos neispravan!")
 
-print(getLineEq(*coords))
+a, b = getLineEq(*coords)
+# print(getFunctionName(a, b))
+plotGraph(lambda x: a*x+b, getFunctionName(a, b), coords, 'PDF')
